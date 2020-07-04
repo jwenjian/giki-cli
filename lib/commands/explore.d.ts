1ae126617f2a968c9d1ea2b0cb78a7c4f3ea1959
Jiang Wenjian <wenjian.jiang@foxmail.com>
2020-07-04 10:19:23 +0800

    release 0.2.0

--- lib/commands/explore.d.ts
+++ lib/commands/explore.d.ts
@@ -1,11 +1,10 @@
 import Command from '../lib/base';
-import { flags } from '@oclif/command';
 import { AxiosInstance } from 'axios';
 export default class Explore extends Command {
     static description: string;
     static flags: {
         help: import("@oclif/parser/lib/flags").IBooleanFlag<void>;
-        number: flags.IOptionFlag<string | undefined>;
+        number: import("@oclif/parser/lib/flags").IOptionFlag<number | undefined>;
     };
     doCommand(userConfig: {
         token: any;
--- lib/lib/base.d.ts
+++ lib/lib/base.d.ts
@@ -1,6 +1,7 @@
 import Command from '@oclif/command';
 import * as axios from 'axios';
 export default abstract class extends Command {
+    initConfig(): Promise<void>;
     run(): Promise<void>;
     doCommand(_userConfig: Object, client: axios.AxiosInstance): Promise<void>;
 }
