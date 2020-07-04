9be6df71e9ca08375aae806d09a26a2e45902386
Jiang Wenjian <wenjian.jiang@foxmail.com>
2020-07-04 11:17:47 +0800

    show tags of talk in explore

--- src/commands/explore.ts
+++ src/commands/explore.ts
@@ -8,7 +8,7 @@ export default class Explore extends Command {
   static description = 'explore talks on giki.app'
 
   static flags = {
-    help: flags.help({char: 'h'}),
+    help: flags.help({ char: 'h' }),
     // flag with a value (-n, --number=VALUE)
     number: flags.integer({ char: 'n', description: 'number of talks to explore, [1-20], default 5' })
   }
@@ -19,7 +19,7 @@ export default class Explore extends Command {
     let n = 5
     if (flags.number) {
       let input_n = flags.number
-      if (input_n >=1 && input_n <= 20) {
+      if (input_n >= 1 && input_n <= 20) {
         n = input_n
       } else if (input_n > 20) {
         n = 20
@@ -39,7 +39,12 @@ export default class Explore extends Command {
     // sort by id desc
     let origData = resp.data.data
     origData.slice(0, n).reverse().forEach((talk: any) => {
-      this.log(`${chalk.blue(talk.name)}\t${chalk.redBright(talk.created_at)}:\n${talk.text}\n------------------------------\n`)
+      this.log(`${chalk.blue(talk.name)}\t${chalk.redBright(talk.created_at)}:`)
+      if (talk.tags && talk.tags.length > 0) {
+        const tag_str = talk.tags.map((t: any) => '#' + t + '#').reduce((a: any, b: any) => a + ', ' + b)
+        this.log(`${chalk.yellow(tag_str)}`)
+      }
+      this.log(`${talk.text}\n------------------------------\n`)
     });
 
   }
