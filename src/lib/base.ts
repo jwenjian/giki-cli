1a6f015f39412bf5854f8a4d358340cb3e7f1102
Jiang Wenjian <wenjian.jiang@foxmail.com>
2020-07-04 10:18:07 +0800

    optimistic config flow

--- src/lib/base.ts
+++ src/lib/base.ts
@@ -2,17 +2,40 @@ import Command, {flags} from '@oclif/command'
 import * as fs from 'fs'
 import {join} from 'path'
 import * as axios from 'axios'
+import { cli } from 'cli-ux'
+const chalk = require('chalk');
 
 
 export default abstract class extends Command {
+  async initConfig() {
+    this.log(`
+    hi, welcome to giki-cli!
+    before you can go ahead, we need to do some config.
+    
+    please firstly go to https://<your_name>.giki.app/setting to get and copy your token,
+    `)
+    const token = await cli.prompt('paste your token here')
+    this.log(chalk.yellow('got it, your token will only appears in local config file, relax.'))
+    const userConfig = {
+      token: token
+    }
+    cli.action.start(`creating config.json in ${this.config.configDir}`)
+    fs.writeFileSync(join(this.config.configDir, 'config.json'), JSON.stringify(userConfig))
+    cli.action.stop()
+    this.log(chalk.green('config done, enjoy!'))
+  }
+
   async run() {
     if (!fs.existsSync(join(this.config.configDir, 'config.json'))) {
-      throw new Error('No config.json')
+      await this.initConfig()
     }
-    const fileContent = fs.readFileSync(join(this.config.configDir, 'config.json'), 'utf8')
+    const configPath = join(this.config.configDir, 'config.json')
+    const fileContent = fs.readFileSync(configPath, 'utf8')
     const userConfig = JSON.parse(fileContent)
     if (!userConfig.token) {
-      throw new Error('Missing token config item in cofig.json')
+      this.log(chalk.bold.red(`missing 'token' filed in ${configPath}`))
+      this.log(chalk.yellow('please delete above file and run again'))
+      return
     }
     const client = axios.default.create({
       baseURL: 'https://giki.app/api',
