4a37cb5e8461ed0080aedfb13a9766958b7ae129
Jiang Wenjian <wenjian.jiang@foxmail.com>
2020-07-05 18:45:31 +0800

    ignore generated lib/*

--- lib/lib/base.d.ts
+++
@@ -1,7 +0,0 @@
-import Command from '@oclif/command';
-import * as axios from 'axios';
-export default abstract class extends Command {
-    initConfig(): Promise<void>;
-    run(): Promise<void>;
-    doCommand(_userConfig: Object, client: axios.AxiosInstance): Promise<void>;
-}
